7ce65b532f4b98ca20e09c6437e561f0b603c914
Chocmanblack <leonardojacum@gmail.com>
2025-02-28 10:37:07 -0500

    add suma de grocery

--- calcularArea/calcular_area.js
+++ calcularArea/calcular_area.js
@@ -8,3 +8,14 @@ function calculateArea(){
     let area = length * width;
     document.getElementById('result').innerText =`El area del rectangulo es: ${area}`;
 }
+
+function groceryTracker(){
+    let gro1 = parseFloat(document.getElementById('grocery1').value); 
+    let gro2 = parseFloat(document.getElementById('grocery2').value); 
+    let gro3 = parseFloat(document.getElementById('grocery3').value); 
+
+    let Total_grocery = gro1 + gro2 + gro3;
+
+    document.getElementById("result_grocery").innerText = `La suma total de los alimentos es: ${Total_grocery}`
+
+}
