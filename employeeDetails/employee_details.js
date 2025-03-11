48ff9fa673ea14d9444b5db06a4f7704c4eaf230
Chocmanblack <leonardojacum@gmail.com>
2025-03-11 12:37:45 -0400

    find employee by specialization

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'Leo Acum', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Niko Malek', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Seba Simpson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'Leo Acum', age: 30, department: 'IT', salary: 50000 , specialization: 'Javascript'},
+    { id: 2, name: 'Niko Malek', age: 28, department: 'HR', salary: 45000 , specialization: 'Python'},
+    { id: 3, name: 'Seba Simpson', age: 35, department: 'Finance', salary: 60000, specialization: 'C++' },
 ];
 
 function displayEmployees(){
@@ -24,9 +24,19 @@ function displayHREmployees(){
 function findEmployeeById(employeeId){
     const foundEmployee = employees.find(employee => employee.id === employeeId);
     if (foundEmployee){
-        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary} Boton final</p>`;
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
 
     }else{
         document.getElementById('employeesDetails').innerHTML = 'no se ha encontrado ningún empleado con este ID';
+    }
+}
+
+function findSpecialization(){
+    const foundSpe = employees.find(employee => employee.specialization === 'Javascript');
+    if (foundSpe){
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundSpe.id}: ${foundSpe.name} - ${foundSpe.department} - ${foundSpe.specialization} Boton final</p>`;
+
+    }else{
+        document.getElementById('employeesDetails').innerHTML = 'no se ha encontrado ningún empleado con esa epecificación';
     }
 }
