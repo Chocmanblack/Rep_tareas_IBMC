c63294cd499734d92db95627b0a068687d6239c7
chocmanblack <leonardojacum@gmail.com>
2025-03-13 11:22:49 -0400

    Speed Analysis

---
+++ speedAnalysis/speed_analysis.js
@@ -0,0 +1,41 @@
+let testText = "La palabra mas rápida del oeste."
+let startTime, endTime;
+
+function startTest(){
+    document.getElementById('inputText').value = testText;
+
+    document.getElementById('output').innerHTML = "";
+    startTime = new Date().getTime();
+
+    var button = document.getElementById("btn");
+    button.innerHTML = "Finalizar Prueba";
+    button.onclick = endTest;
+}
+
+function endTest(){
+    endTime = new Date().getTime();
+
+    document.getElementById('userInput').readOnly = true;
+
+    var timeElapsed = (endTime - startTime) / 1000;
+    var userTypedText = document.getElementById('userInput').value;
+
+    var typeWords = userTypedText.split(/\s+/).filter(word => word !== "").length;
+
+    var wpm = 0;
+
+    if(timeElapsed !== 0 && !isNaN(typeWords)){
+        wpm = Math.round((typeWords / timeElapsed) * 60);
+
+    }
+
+    var outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = '<h2> Resultados de la prueba de escritura:</h2>'+
+        "<p>Palabras Escribidas: " + typeWords + '</p>' +  
+        "<p>Tiempo Transcurrido: " + timeElapsed.toFixed(2) + ' segundos</p>' +  
+        "<p>Palabras Por Minuto (WPM): " + wpm + '</p>';  
+    
+    var button = document.getElementById('btn');
+    button.innerHTML = "Iniciar Prueba";
+    button.onclick = startTest;
+}
